e090770385a81dcb5b9840b204e1f8e25ed871fa
Emmanuel Cruzat <etc@njit.edu>
2026-05-24 17:50:48 -0400

    First version of node server

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("kokou-stats API is running");
+});
+
+app.get("/player/:username", async (req, res) => {
+  const username = req.params.username;
+  // basic route to obtain a user that we want to fetch stats for
+
+  const response = await axios.get(
+    `https://api.worldofwarships.com/wows/account/list/?application_id=${process.env.WOWS_API_KEY}&search=${username}`,
+  );
+
+  const accountId = response.data.data[0].account_id;
+  const accountData = await axios.get(
+    `https://api.worldofwarships.com/wows/account/info/?application_id=${process.env.WOWS_API_KEY}&account_id=${accountId}`,
+  );
+  res.send(accountData.data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
