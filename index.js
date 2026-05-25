dc9dc12e1c9163e43328baa37d21203a1fa6c7c6
Emmanuel Cruzat <etc@njit.edu>
2026-05-25 10:44:12 -0400

    added temporary endpoint to get random battle winrate directly

--- index.js
+++ index.js
@@ -23,6 +23,25 @@ app.get("/player/:username", async (req, res) => {
   res.send(accountData.data);
 });
 
+// temporary route to get a player's winrate
+app.get("/player/:username/wr", async (req, res) => {
+  const username = req.params.username;
+  // basic route to obtain a user that we want to fetch stats for
+
+  const response = await axios.get(
+    `https://api.worldofwarships.com/wows/account/list/?application_id=${process.env.WOWS_API_KEY}&search=${username}`,
+  );
+
+  const accountId = response.data.data[0].account_id;
+  const accountData = await axios.get(
+    `https://api.worldofwarships.com/wows/account/info/?application_id=${process.env.WOWS_API_KEY}&account_id=${accountId}`,
+  );
+  res.send(
+    accountData.data.data[accountId].statistics.pvp.wins /
+      accountData.data.data[accountId].statistics.pvp.battles,
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
